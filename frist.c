ce7786cb4e13d15ea0aaba3e8ba7b0037b63af8b
Seli Adzo Adih <adzoseli06@gmail.com>
2025-12-05 17:31:04 +0000

    Create frist.c

---
+++ frist.c
@@ -0,0 +1,53 @@
+/*
+Finally my very first c program
+*///Day one of learning C programming language
+#include <stdio.h>
+int main()
+{
+/*printf("Hello world!\t");
+printf("Have a nice day!\n");
+printf("My name is Seli.\n");
+printf("I am learning C programming language.\nAnd it is awesome!\n");
+printf("C programming is fun!\n\n");
+int myAge = 19;
+float myCWA = 78.5;
+char myGrade = 'A'; 
+printf("I am %d years old.\n", myAge); //Displaying age
+printf("My current CWA is %.2f.\n", myCWA); //Displaying CWA
+printf("My grade in C programming is %c.\n", myGrade); //Displaying grade
+printf("I am %d years old, my current CWA is %.2f and my grade in C programming is %c.\n", myAge, myCWA, myGrade); //Displaying all information together
+printf("I am %d years old.\n ",19);
+int myNum = 35;
+int myOtherNum = 23;
+myOtherNum = myNum;
+printf("%d",myOtherNum);*/
+
+//Day two of programming in C
+/*int x = 34;
+int y = 48;
+int sum = x + y;
+printf("%d", sum);
+int x, y, z;
+x = y = z = 50;
+printf("%d",x + y + z);
+
+// Student data
+int studentID = 15;
+int studentAge = 23;
+float studentFee = 75.25;
+char studentGrade = 'B';
+
+// Print variables
+printf("Student id: %d\n", studentID);
+printf("Student age: %d\n", studentAge);
+printf("Student fee: %.2f\n", studentFee);
+printf("Student grade: %c", studentGrade);*/
+
+//Calculating the area of a circle
+int radius = 7;
+int pi = 3.14159;
+
+float area = pi * radius * radius;
+printf("The area of the circle is %f",area);
+return 0;
+}
